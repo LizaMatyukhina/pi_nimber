b8d8979ed11b698f241591d76792c2331307498e
LizaMatyukhina <44421806+LizaMatyukhina@users.noreply.github.com>
2019-09-10 13:11:51 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,131 @@
+import threading
+from multiprocessing import Process
+from queue import Queue
+import requests
+import time
+import re
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+class Sequence(Process):
+
+    def __init__(self, url="http://www.eveandersson.com/pi/digits/1000000"):
+        super().__init__()
+        self.main = requests.get(url)
+        self.main = self.main.text
+
+        with open('dates.txt', 'r') as f_in:
+            self.birth = f_in.readlines()
+
+    def word_processing(self):
+        self.main = re.sub('\n', '', self.main)
+        self.match = re.findall(r'<pre>(3\.\d+)', self.main)
+        self.pi = self.match[0]
+
+    def main_(self):
+        in_ = 0
+        out_ = 0
+        for line in self.birth:
+            line = line.replace('/', '')
+            line = line.strip()
+            if line in self.pi:
+                in_ += 1
+            else:
+                out_ += 1
+
+        print('There are', in_, 'birthdays in number Pi')
+
+    def run(self):
+        self.word_processing()
+        self.main_()
+
+
+class Queues(Sequence):
+
+    def __init__(self):
+        super().__init__()
+        self.in_ = 0
+
+    def quest(self, q):
+        while True:
+            item = q.get()
+            if item is None:
+                break
+            item = str(item)
+            if item in self.pi:
+                self.in_ += 1
+
+        return self.in_
+
+    def run(self):
+        self.word_processing()
+
+        q = Queue()
+        for line in self.birth:
+            line = line.replace('/', '')
+            line = line.strip()
+            q.put(line)
+        q.put(None)
+        q.put(None)
+        q.put(None)
+
+        with ThreadPoolExecutor(max_workers=3) as pool:
+            results = [pool.submit(self.quest, q)]
+
+            for future in as_completed(results):
+                print('There are', future.result(), 'birthdays in number Pi')
+
+
+class Block(Sequence):
+
+    def __init__(self):
+        super().__init__()
+        self._in = 0
+
+    def main_(self):
+        lock = threading.RLock()
+        for line in self.birth:
+            line = line.replace('/', '')
+            line = line.strip()
+
+            if line in self.pi:
+                lock.acquire()
+                self._in += 1
+                lock.release()
+
+        return self._in
+
+    def run(self):
+        self.word_processing()
+
+        with ThreadPoolExecutor(max_workers=3) as pool:
+            results = [pool.submit(self.main_)]
+
+            for future in as_completed(results):
+                print('There are', future.result(), 'birthdays in number Pi')
+
+
+if __name__ == '__main__':
+    time_now = time.time()
+    proc = Sequence()
+    proc.start()
+    proc.join()
+    print('Time for sequential execution: ',
+          round(time.time() - time_now, 5),
+          'sec.', '\n')
+
+    time_now = time.time()
+    proc = Queues()
+    proc.start()
+    proc.join()
+    print('Time for queue: ',
+          round(time.time() - time_now, 5),
+          'sec.', '\n')
+
+    time_now = time.time()
+    proc = Block()
+    proc.start()
+    proc.join()
+    print('Time for execution with block: ',
+          round(time.time() - time_now, 5),
+          'sec.', '\n')
